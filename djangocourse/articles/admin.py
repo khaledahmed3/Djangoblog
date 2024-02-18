08bc52a0cc77f5c4b9dc6ccd9ab709164213887b
khaledahmed3 <khaledahmed512@gmail.com>
2024-02-18 22:53:19 +0800

    adding in lesson 10 "admin panel"

--- djangocourse/articles/admin.py
+++ djangocourse/articles/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from .models import Article
 
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'date')
-    list_filter = ('date',)
-    search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
-
-admin.site.register(Article, ArticleAdmin)
+admin.site.register(Article)
--- djangocourse/articles/views.py
+++ djangocourse/articles/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
 def article_list(request):
-    return render(request,'articles/articles_list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request,'articles/articles_list.html', {'articles': articles})
+
+hello zeby
